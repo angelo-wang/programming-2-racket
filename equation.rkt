f7aa50ea84f7a1db5857b802a934347c7e059a6e
angelo wang <angelowang11@outlook.com>
2022-02-08 09:14:00 -0800

    Added the rest of the notes files. Added the exercise files as well

---
+++ equation.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname equation) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(+ (sqr 4) (/ 14 (* 2 3.5)) (* 12 4))
+(+ 16      (/ 14 (* 2 3.5)) (* 12 4))
+(+ 16      (/ 14 7)         (* 12 4))
+(+ 16      2                (* 12 4))
+(+ 16      2                48      )
+66
